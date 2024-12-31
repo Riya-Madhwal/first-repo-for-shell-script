3fa205672ff7f59dfe005cbc92706134c632f220
Riya-Madhwal <165165769+Riya-Madhwal@users.noreply.github.com>
2024-12-31 19:17:54 +0530

    Update archive_project.sh

--- archive_large_old_files/archive_project.sh
+++ archive_large_old_files/archive_project.sh
@@ -1,1 +1,34 @@
+#!/bin/bash
 
+#variables
+directory=/home/riya/tutorials/find_command
+days=10
+depth=1
+RUN=0
+
+#check if directory present or not
+
+if [! -d $directory ]
+then
+     echo "directory does not exist: $BASE"
+	 exit 1
+	 
+fi
+
+#create archive folder if not present
+if [! -d $directory/archive ]
+then
+    mkdir $directory/archive
+fi
+
+#find the list of files larger than 1GB
+
+for i in 'find $directory -maxdepth $DEPTH -type f -size +1G'
+do 
+ if [$RUN -eq 0 ]
+  then 
+   echo "moving  files from $directory/ to $directory/archive location"
+   gzip $i || exit 1
+   mv $i.gz $directory/archive || exit 1
+ fi
+done
